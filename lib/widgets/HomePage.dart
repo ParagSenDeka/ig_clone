dfd47b9ff6c3f2729a105f3bbf15525afc7367ee
Parag Deka <96935811+ParagSenDeka@users.noreply.github.com>
2025-02-18 20:41:03 +0530

    Init

--- lib/widgets/HomePage.dart
+++ lib/widgets/HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ig_clone/widgets/Navbar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,8 +9,17 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Instagram"),
+        title: Text("Instagram",
+          style: GoogleFonts.lato(),
+        ),
+        actions: [
+          IconButton(onPressed: () {
+            
+          }, icon: Icon(Icons.heart_broken_outlined)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.chat_bubble_outline))
+        ],
       ),
+      bottomNavigationBar: Navbar(),
     );
   }
 }
