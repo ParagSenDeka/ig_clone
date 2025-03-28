71ce43fdc2bdaaba86bc347beb276c09b1977906
Parag Deka <96935811+ParagSenDeka@users.noreply.github.com>
2025-03-28 22:06:23 +0530

    Did something

--- lib/pages/UploadPage.dart
+++ lib/pages/UploadPage.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("This is a home page"),
+      child: Text("This is a upload page"),
     );
   }
 }
