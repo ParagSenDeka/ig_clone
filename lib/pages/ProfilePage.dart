21c935fe14e8298c2070bf4641a2755ac1a76076
Parag Deka <96935811+ParagSenDeka@users.noreply.github.com>
2025-03-28 22:11:51 +0530

    Fix

--- lib/pages/ProfilePage.dart
+++ lib/pages/ProfilePage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class Profilepage extends StatelessWidget {
+  const Profilepage({super.key});
 
   @override
   Widget build(BuildContext context) {
