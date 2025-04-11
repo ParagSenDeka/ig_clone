c8e5ea063a9321d7d93fdd3d5556111b9e594824
Parag Deka <96935811+ParagSenDeka@users.noreply.github.com>
2025-04-11 21:26:49 +0530

    Update Backend

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -2,8 +2,8 @@ import 'package:supabase/supabase.dart';
 
 Future<void> login() async {
   final supabase = SupabaseClient(
-    'https://pstmxxthcsgcbtjhxdbp.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzdG14eHRoY3NnY2J0amh4ZGJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzODQwNTcsImV4cCI6MjA1OTk2MDA1N30.5UN7nqdIFTIbNXzxKA3Dcli_QnwTB43RUohBeD4Pg6Q',
+    'your url',
+    'your key',
   );
 
   final email = 'abc@gmail.com';
